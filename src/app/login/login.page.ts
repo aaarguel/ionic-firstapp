645cd5ed880e6500384cdc3430bfd1c24613bbc5
Leiber Rivas <lvrivas@espol.edu.ec>
2020-05-30 22:04:22 -0500

    Validators

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,28 +5,32 @@ import { doc_users} from '../interfaces/doc-users';
 import {Validators, FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { map, take, debounceTime } from 'rxjs/operators';
-
+export class CustomValidator {
+  static username(afs: AngularFirestore) {
+    return (control: AbstractControl) => {
+      const username = control.value.toLowerCase();
+      return afs.collection('usuarios', ref=> ref.where('username','==',username))
+      .valueChanges().pipe(
+        debounceTime(500),
+        take(1),
+        map(arr=>arr.length?{usernameAvailable:false}:null)
+      )
+    }
+  }
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
+
+
 export class LoginPage implements OnInit {
   usuario: string;
   contrasena: string;
   document: doc_users;  
-
-  loginForm = this.formBuilder.group({
-    username : ['',[Validators.required, this.firebase.getUserbyUserName(this.usuario)]],
-    password : ['',[Validators.required]]
-  });
-
-  public errorMessages = {    
-    password: [
-      { type: 'required', message: 'Se necesita la contraseña' }      
-    ],
-  };
-
+  loginForm: FormGroup;
 
   constructor(private router: Router,
               private afs: AngularFirestore,
@@ -35,6 +39,29 @@ export class LoginPage implements OnInit {
     
   }
 
+  ngOnInit() { 
+    this.initForm();
+    this.firebase.getUsers().subscribe(usuarios=>{ //Puede cambiar por then
+      console.log(usuarios);
+    });
+  }
+
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      username : ['',[Validators.required, CustomValidator.username(this.afs)]],
+      password : ['',[Validators.required]]
+    });
+  }
+
+  public errorMessages = {    
+    password: [
+      { type: 'required', message: 'Se necesita la contraseña' }      
+    ],
+  };
+
+  change(ev) {
+    console.log(this.loginForm.controls['username'].errors)
+  }
   get username() {
     return this.loginForm.get("username");
   }
@@ -58,13 +85,6 @@ export class LoginPage implements OnInit {
 
   public submit() {
     console.log(this.loginForm.value);
-  }
-
-  ngOnInit() { 
-    this.firebase.getUsers().subscribe(usuarios=>{ //Puede cambiar por then
-      console.log(usuarios);
-    });
-  }
-    
+  }    
 }
 
